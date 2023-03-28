b0b26c69a9ef89d9d7e1a07f0b7b2aa4bfb98f43
senivlm <91743906+senivlm@users.noreply.github.com>
2023-03-28 10:53:26 +0300

    Update Program.cs

--- Home_task_1/SpiralNumbers/Program.cs
+++ Home_task_1/SpiralNumbers/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace SpiralNumbers
 {
     internal class Program
-    {
+    {//Вітаю. Перше завдання по створенню репозиторію Ви виконали, але не дотримали вимогу щодо найменування підзадач.
         static void Main(string[] args)
         {
             ProgramPresenter.ExecuteTaskExample();
